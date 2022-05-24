1de892d6f6722e2879c3285a7664a2063ebba80b
LeonardoSalomao <leonardo.salomao@bandtec.com.br>
2022-05-24 20:15:19 -0300

    Script

---
+++ script.sh
@@ -0,0 +1,53 @@
+            clear
+            echo "Atualizando pacotes do sistema(1/4)"
+            sleep 3
+            sudo apt update && sudo apt upgrade -y
+
+            sudo apt install zip -y
+
+            curl -s "https://get.sdkman.io" | bash
+
+            source "/home/$USER/.sdkman/bin/sdkman-init.sh"
+            clear
+            echo "Instalando java(2/4)"
+            sleep 3
+            sudo apt install openjdk-11-jre -y
+            
+            sudo apt update && sudo apt upgrade -y
+            clear
+            echo "Instalando docker(3/4)"
+            sleep 3
+            sudo apt install docker.io -y
+
+            sudo apt install docker-compose -y
+
+            sudo apt-get install \
+            ca-certificates \
+            curl \
+            gnupg \
+            lsb-release -y
+
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+             $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+            sudo apt-get update
+
+            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+            sudo apt-get update
+
+            sudo systemctl enable docker
+
+            clear
+
+            echo "Fazendo download do software smart-sac(4/4)"
+            sleep 3
+
+            git clone https://github.com/LeonardoSalomao/Script_Instalacao.git
+            clear
+            echo "Instalação efetuada com sucesso!"
+            read -p "Pressione Enter para continuar"
+            echo "================================================"
---
+++ script.sql
@@ -0,0 +1,77 @@
+CREATE TABLE empresas (
+	id_empresa 				INT PRIMARY KEY AUTO_INCREMENT,
+    nome_empresa			VARCHAR(45),
+    cnpj					VARCHAR(15),
+    email					VARCHAR(50),
+	senha					VARCHAR(45),
+    telefone				VARCHAR(15),
+    logradouro				VARCHAR(80),
+    numero					INT,
+    cep						VARCHAR(9),
+    cidade					VARCHAR(30),
+    estado					CHAR(2)
+    );
+    
+CREATE TABLE avaliacoes (
+	id_avaliacao			INT PRIMARY KEY AUTO_INCREMENT,
+    nome_avaliador			VARCHAR(55),
+    mensagem				VARCHAR(255),
+    data_avaliacao			DATE,
+    qtd_estrelas			INT,
+    fk_empresa				INT,
+    -- FOREIGN KEY (fk_empresa) REFERENCES empresas (id_empresa)
+    );
+    
+CREATE TABLE operacoes (
+	id_operacao				INT PRIMARY KEY AUTO_INCREMENT,
+    nome_operacao			VARCHAR(45),
+    localidade				VARCHAR(20),
+    nome_gerente			VARCHAR(45),
+    tel_gerente				VARCHAR(15),
+    email_gerente			VARCHAR(50),
+    senha_gerente			VARCHAR(45),
+    fk_empresa				INT,
+    -- FOREIGN KEY (fk_empresa) REFERENCES empresas (id_empresa)
+    );
+    
+CREATE TABLE maquinas (
+	id_maquina 				INT PRIMARY KEY AUTO_INCREMENT,
+    hostname				VARCHAR(45),
+    serial_maquina			VARCHAR(45),
+    localidade_maquina		VARCHAR(30),
+    fk_operacao				INT,
+    -- FOREIGN KEY (fk_operacao) REFERENCES operacoes (id_operacao)
+    );
+
+CREATE TABLE log_cliques (
+	id_clique 				INT PRIMARY KEY AUTO_INCREMENT,
+    cliques					VARCHAR(15),
+    data_clique				DATE,
+    fk_maquina				INT,
+    -- FOREIGN KEY (fk_maquina) REFERENCES maquinas (id_maquina)
+    );
+    
+CREATE TABLE componentes (
+	id_componente			INT PRIMARY KEY AUTO_INCREMENT,
+    tipo					VARCHAR(15)
+    );
+
+CREATE TABLE log_registros (
+	id_log_registro 		INT PRIMARY KEY auto_increment,
+    uso						DOUBLE,
+    disponivel				DOUBLE,
+    frequencia				DOUBLE,
+    fk_componente			INT,
+    -- FOREIGN KEY (fk_componente) REFERENCES componentes (id_componente),
+    -- fk_maquina				INT,
+    -- FOREIGN KEY (fk_maquina) REFERENCES maquinas (id_maquina),
+    --   data_hora				DATETIME
+      );
+      
+CREATE TABLE log_alertas (
+	id_log_alerta			INT PRIMARY KEY AUTO_INCREMENT,
+    codigo_urgencia			VARCHAR(15),
+    descricao				VARCHAR(255),
+    fk_registro				INT,
+    -- FOREIGN KEY (fk_registro) REFERENCES log_registros (id_log_registro)
+    );
